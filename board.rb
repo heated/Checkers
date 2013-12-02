1ed19d103beba7c376702b467538555fcce337c5
Edward <edabsw@gmail.com>
2013-12-02 13:35:50 -0800

    Phase 1 done; Phase 2 halfway done

--- board.rb
+++ board.rb
@@ -3,9 +3,13 @@ require 'colorize'
 
 class Board
 	def initialize
-		@grid = Array.new(8) { Array.new(8) }
+    init_board
 		init_pieces
 	end
+
+  def init_board
+    @grid = Array.new(8) { Array.new(8) }
+  end
 
 	def init_pieces
 		[true, false].each do |first_player|
@@ -13,7 +17,7 @@ class Board
 				8.times do |col|
 					color = first_player ? :b : :r
 					mod_row = first_player ? row + 5 : row
-					Piece.new(color, [mod_row, col], self) if (row + col).even?
+					Piece.new(color, [col, mod_row], self) if (col + mod_row).odd?
 				end
 			end
 		end
@@ -30,27 +34,36 @@ class Board
 	end
 
 	def move(pos1, pos2)
-		
+		unless self.empty?(pos1)
+			piece = self[pos1]
+			# TODO
+		end
 	end
 
 	def empty?(pos)
-		self[pos].nil?
+		on_board?(pos) && self[pos].nil?
+	end
+
+	def on_board?(pos)
+		pos.all? { |coord| (0...8).include?(coord) }
+	end
+
+	def enemy?(pos, color)
+		on_board?(pos) && !empty?(pos) && self[pos].color != color
 	end
 
 	def to_s
 		str = ""
-		@grid.each do |row|
-			row.each do |piece|
-				unless piece.nil?
-					if piece.color == :b
-						str << piece.to_s.black
-					else
-						str << piece.to_s.red
-					end
-				else
-					str << " "
-				end
-				str << "|"
+		@grid.size.times do |x|
+			@grid.size.times do |y|
+				# account for terminal output
+				piece = @grid[y][x] 
+
+				new_str = (piece.nil? ? " " : piece.to_s)
+
+				new_str += " "
+
+        str << ((x + y).even? ? new_str.black.on_white : new_str.black.on_green)
 			end
 			str << "\n"
 		end
@@ -60,5 +73,15 @@ class Board
 end
 
 board = Board.new
+
+pawn = board[[0, 5]]
+enemy = board[[1, 2]]
+
+pawn.perform_moves!([[1, 4]])
+pawn.perform_moves!([[2, 3]])
+
+enemy.perform_moves!([[3, 4]])
+
+# board.move([5, 1], [4, 2])
 
 puts board
